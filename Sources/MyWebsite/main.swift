a1841c90b0811a57d754f3f91343320499935041
Josh Prewer <josh.prewer@focusrite.com>
2020-10-03 13:35:31 +0100

    Cleanup work cards

--- Sources/MyWebsite/main.swift
+++ Sources/MyWebsite/main.swift
@@ -133,21 +133,42 @@ private extension Node where Context == HTML.BodyContext {
         return .div(.class("work"),
                     .h1("Work"),
                     .div(.class("work-cards"),
-                         .workCard(colour: "lp", imagePath: "lp-ss.png", iconImagePath: "lp-icon.png", text: "Launchpad"),
-                         .workCard(colour: "gb", imagePath: "gb-ss.png", iconImagePath: "gb-icon.png", text: "Groovebox"),
-                         .workCard(colour: "bw", imagePath: "bw-ss.png", iconImagePath: "bw-icon.png", text: "Blocs Wave")
+                         .workCard(
+                            link: "https://ampifymusic.com/launchpad/",
+                            colour: "lp",
+                            imagePath: "lp-ss.png",
+                            iconImagePath: "lp-icon.png",
+                            text: "Launchpad"),
+                         .workCard(
+                            link: "https://ampifymusic.com/groovebox/",
+                            colour: "gb",
+                            imagePath: "gb-ss.png",
+                            iconImagePath: "gb-icon.png",
+                            text: "Groovebox"),
+                         .workCard(
+                            link: "https://ampifymusic.com/blocswave/",
+                            colour: "bw",
+                            imagePath: "bw-ss.png",
+                            iconImagePath: "bw-icon.png",
+                            text: "Blocs Wave")
                     )
         )
     }
 
-    static func workCard(colour: String, imagePath: String, iconImagePath: String, text:String) -> Node {
-        return .div(.class("card"),
-                    .img(.class("card-image"), .src(imagePath)),
-                    .div(.class("card-text  \(colour)"),
-                         .h2(.text(text)),
-                         .img(.class("card-icon"), .src(iconImagePath))
-                    )
-                )
+    static func workCard(link: String,
+                         colour: String,
+                         imagePath: String,
+                         iconImagePath: String,
+                         text:String) -> Node {
+        return .a(.href(link),
+                  .div(.class("card"),
+                       .img(.class("card-image"), .src(imagePath)),
+                       .div(.class("card-text  \(colour)"),
+                            .h2(.text(text)),
+                            .img(.class("card-icon"), .src(iconImagePath))
+                       )
+                  )
+            )
     }
 
     static func createSocials() -> Node {
